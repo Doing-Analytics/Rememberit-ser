2e0c29748bc3d61fa73a0baa047c9f7f8c195b02
Ning <a24701770@gmail.com>
2023-01-23 01:33:02 +0800

    add aasm to projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,25 @@
 class Project < ApplicationRecord
+
   acts_as_paranoid
   belongs_to :user
+
+  include AASM
+
+
+  aasm timestamps: true do
+    state :pending, initial: true
+    state :unstarted, :in_progress, :completed
+
+    event :unstart do
+      transitions from: :pending, to: :unstarted
+    end
+
+    event :progress do
+      transitions from: :unstarted, to: :in_progress
+    end
+
+    event :complete do
+      transitions from: :in_progress, to: :completed
+    end
+  end
 end
